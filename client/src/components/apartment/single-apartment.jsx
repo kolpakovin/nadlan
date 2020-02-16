82025333a57a6ff9d9cff7af0b336dade9e3f5f1
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-02-16 11:10:18 +0200

    updated apartment page and css

--- client/src/components/apartment/single-apartment.jsx
+++ client/src/components/apartment/single-apartment.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./apartment.css";
-import {Carousel} from "react-bootstrap";
-import {getApartment, getUser} from "../app-data/apartments-server";
-import { Link } from "react-router-dom";
+import { getApartment, getUser } from "../app-data/apartments-server";
 import SingleAppLoader from "./single-app-loader";
+import ApartmentDetails from "./apartment-details";
 
 
 class Apartment extends React.Component {
@@ -21,32 +20,32 @@ class Apartment extends React.Component {
             user: null,
             sideMenu: false,
             showContact: false,
-            images: ["http://localhost:4000/images/apartment-loader.jpg","http://localhost:4000/images/apartment-loader.jpg","http://localhost:4000/images/apartment-loader.jpg", "http://localhost:4000/images/apartment-loader.jpg"]
+            images: ["http://localhost:4000/images/apartment-loader.jpg", "http://localhost:4000/images/apartment-loader.jpg", "http://localhost:4000/images/apartment-loader.jpg", "http://localhost:4000/images/apartment-loader.jpg"]
         }
     }
 
-    componentDidMount = async() =>{
-        getApartment(this.props.match.params.id,this.showApartments)
-        
+    componentDidMount = async () => {
+        getApartment(this.props.match.params.id, this.showApartments)
+
     }
     showApartments = (data) => {
-        this.setState({    
+        this.setState({
             apartment: data.apartment[0],
             loading: false
-        },  () => this.showImages())
+        }, () => this.showImages())
     }
-    showImages = async() => {
+    showImages = async () => {
         const user = await getUser(this.state.apartment.user_id)
-        this.setState({user: user[0]})
-        if(!this.state.apartment.images){
+        this.setState({ user: user[0] })
+        if (!this.state.apartment.images) {
             this.setState({
-                images: [this.state.apartment.main_image, this.state.apartment.main_image, this.state.apartment.main_image,this.state.apartment.main_image]
+                images: [this.state.apartment.main_image, this.state.apartment.main_image, this.state.apartment.main_image, this.state.apartment.main_image]
             })
         } else {
-            if(this.state.apartment.images.split(',').length < 3){
+            if (this.state.apartment.images.split(',').length < 3) {
                 let images = this.state.apartment.images.split(',')
                 images.forEach(image => images.push(image))
-                this.setState({images})
+                this.setState({ images })
             } else {
                 this.setState({
                     images: this.state.apartment.images.split(',')
@@ -61,131 +60,35 @@ class Apartment extends React.Component {
     }
     showContact = (e) => {
         e.preventDefault();
-        this.setState({showContact: true});
+        this.setState({ showContact: true });
     }
     render() {
-        const {apartment, loading, user} = this.state;
+        const { apartment, images, loading, user } = this.state;
         return (
-            loading ? <SingleAppLoader/> :
             <div>
-
-                
-                <div className={"position-relative"}>
-                    <i id={"side-menu-button"} onClick={this.showSideMenu} className="fas fa-address-card"></i>
-                </div>
-                <div id="button-apart">
-                    <Link to={"/apartments"}><button><i className="fas fa-arrow-left"></i> Bact To All Apartments </button></Link>
-                </div>
-                {
-                    this.state.images !== []
-                            &&
-
-                <Carousel className="mt-3">
-
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={this.state.images[0].includes("http") ? this.state.images[0] : "http://localhost:4000/"+this.state.images[0]}
-                            alt="First slide"
-                        />
-                        <div>
-
+                {/* <Header toggleModal={toggleModal} user={this.props.user} toggleUserMenu={toggleUserMenu} activeUserMenu={activeUserMenu} logOutUser={logOutUser} /> */}
+                {loading ? <SingleAppLoader/> :
+                    <div id="single-app">
+                        <div className={'container-fluid'}>
+                            <h4>{apartment.description} <i className="fas fa-medal" /></h4>
                         </div>
-                        <Carousel.Caption>
 
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={this.state.images[1].includes("http") ? this.state.images[1] :"http://localhost:4000/"+this.state.images[1]}
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={this.state.images[2].includes("http") ? this.state.images[2] :"http://localhost:4000/"+this.state.images[2]}
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-
-                        </Carousel.Caption>
-
-                    </Carousel.Item>
-
-
-                </Carousel>
+                        <div className={'row images m-0'} >
+                            <div className={'col-sm-12 col-md-6 p-0 overflow-hidden'} style={{ height: '50vh' }}>
+                                <img height={'100%!important'} src={`http://localhost:4000/${images[0]}`} alt={""} />
+                            </div>
+                            <div className={'col-sm-12 col-md-6 p-0'}>
+                                <div className={'row m-0'}>
+                                    <div className={'col-6 p-0 overflow-hidden'} style={{ height: '25vh' }}><img height={'100%'} width={'100%'} src={`http://localhost:4000/${images[1]}`} alt={""} /></div>
+                                    <div className={'col-6 p-0 overflow-hidden'} style={{ height: '25vh' }}><img height={'100%'} width={'100%'} src={`http://localhost:4000/${images[2]}`} alt={""} /></div>
+                                    <div className={'col-6 p-0 overflow-hidden'} style={{ height: '25vh' }}><img height={'100%'} width={'100%'} src={`http://localhost:4000/${images[3]}`} alt={""} /></div>
+                                    <div className={'col-6 p-0 overflow-hidden'} style={{ height: '25vh' }}><img height={'100%'} width={'100%'} src={`http://localhost:4000/${images[4]}`} alt={""} /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <ApartmentDetails apartment={{ ...apartment }} user={{ ...user}}/>
+                    </div>
                 }
-
-                <div className={"d-flex position-relative description"}>
-                    <p className={"upload-time-1 position-absolute"}>{apartment.description}</p>
-                    <p className={"ml-2 rent-status-1 position-absolute"}>{apartment.sale_status === 'both' ? 'For Rent and For Sale' :"For " + apartment.sale_status}</p>
-
-                </div>
-                <div className={"apartment-details"}>
-                    <p><b style={{fontSize: "22px"}}>${Math.floor(apartment.price)}</b></p>
-                    <span>{(apartment.number_of_bath === 1) ? (apartment.number_of_bath + " baths") : (apartment.number_of_bath + " baths")}</span>
-                    <span
-                        className={"ml-4"}>{(apartment.number_of_room === 1) ? (apartment.number_of_room + " room") : (apartment.number_of_room + " rooms")}</span>
-                    <span className={"ml-4"}>{apartment.sqft && (apartment.sqft + " sqft")}</span>
-
-                    <p>Address: {(apartment.address)}</p>
-                    <p>City: {(apartment.real_city_name)}</p>
-
-                </div>
-
-
-                        <div className={"google-map"}>
-                            <iframe title="myFrame" src={`https://maps.google.com/maps?q=${apartment.real_city_name.replace(" ", "")}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
-                                    style={{border:"0"}}/>
-                        </div>
-                {/*<i className="fas fa-angle-right" style={{fontSize: "44px"}}></i>*/}
-
-
-
-                <div id={"div-form"} className={this.state.sideMenu ? "active" : undefined}>
-                    {!this.state.showContact &&
-                        <label onClick={e => this.showContact(e)} className={"contact"}>Person To Contact</label>}
-                    { this.state.showContact &&
-                        <div id="contact">
-                            <p >{`Contact: ${user.first_name}`}</p>
-                            <p >{`Phone: ${user.phone}`}</p>
-                        </div> 
-                        }
-                    {/* <div className={"text-center"} id={"phone-form"}>
-                        <form action="">
-                            More about this property
-                            <div className={"d-table  mb-2 div-input"}><span className="input-group-addon">
-                                <i className="far fa-user"/></span>
-                                <input id={"user-name-input"} type="text" placeholder={"Your Name"}/></div>
-                            <div className={"d-table  mb-2 div-input"}><span className="input-group-addon"><i
-                                className="fas fa-envelope"/></span>
-                                <input id={"user-name-input"} type="text" placeholder={"Email"}/></div>
-                            <div className={"d-table  mb-2 div-input"}><span className="input-group-addon"><i
-                                className="fas fa-phone-alt"/></span>
-                                <input id={"user-name-input"} type="text" placeholder={"Phone"}/></div>
-                            <div><textarea defaultValue={"I'm interested in"}  id="text-input-area" cols="20" rows="2"></textarea>
-                            </div>
-                            <div>
-                                <button id={"form-button"} type="button">Email Agent</button>
-                            </div>
-                            <div className={"text-justify mt-2"} style={{fontSize: "13px"}}><span>By proceeding, you consent to receive calls and texts at the number you provided,
-                                including marketing by autodialer and prerecorded and artificial voice, and email,
-                                from realtor.com and others about your inquiry and other home-related matters,
-                                but not as a condition of any purchase</span></div>
-
-                        </form>
-
-                    </div> */}
-                </div>
-
-
             </div>
         )
     }
